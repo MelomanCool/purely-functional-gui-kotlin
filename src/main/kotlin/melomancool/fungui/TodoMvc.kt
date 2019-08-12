c74ae2cf6c153680cd37ca865b9684cef2ef1e0a
MelomanCool <melomancool@gmail.com>
2019-08-13 00:36:28 +0400

    Todo MVC: Add buttons to delete todos.

--- src/main/kotlin/melomancool/fungui/TodoMvc.kt
+++ src/main/kotlin/melomancool/fungui/TodoMvc.kt
@@ -16,10 +16,16 @@ data class Model(val newTodoText: String, val todos: List<Todo>)
 sealed class Msg
 data class SetNewTodoText(val text: String): Msg()
 object AddNewTodo: Msg()
+data class DeleteTodo(val id: Int): Msg()
 
 fun view(model: Model): View<Msg> =
     VerticalLayout(
-        *model.todos.map { Label(it.text) }.toTypedArray(),
+        *model.todos
+            .mapIndexed { i, it -> HorizontalLayout(
+                Label(it.text),
+                Button(text = "X", onClick = DeleteTodo(id = i))
+            )}
+            .toTypedArray(),
         HorizontalLayout(
             TextField(label = "", text = model.newTodoText, onInput = ::SetNewTodoText),
             Button(text = "Add Todo", onClick = AddNewTodo)
@@ -39,6 +45,8 @@ fun update(msg: Msg, model: Model): Model =
             } else {
                 model
             }
+        is DeleteTodo ->
+            model.copy(todos = model.todos.filterIndexed{ i, _ -> i != msg.id })
     }
 
 fun main() {
