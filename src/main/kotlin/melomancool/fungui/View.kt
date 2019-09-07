7e73c4f65fc36acba7b750fd9afe942748caf0b6
MelomanCool <melomancool@gmail.com>
2019-09-08 03:41:12 +0400

    Separate View types into Elements and Layouts.

--- src/main/kotlin/melomancool/fungui/View.kt
+++ src/main/kotlin/melomancool/fungui/View.kt
@@ -2,31 +2,35 @@ package melomancool.fungui
 
 sealed class View<out Mes>
 
+sealed class Element<Mes>: View<Mes>()
+sealed class Layout<Mes>: View<Mes>()
+
 data class Button<Mes>(
     val text: String,
     val onClick: Mes? = null
-): View<Mes>()
+): Element<Mes>()
 
-data class Label(val text: String): View<Nothing>()
+data class Label(val text: String): Element<Nothing>()
 
-data class VerticalLayout<Mes>(val children: List<View<Mes>>): View<Mes>() {
+data class VerticalLayout<Mes>(val children: List<View<Mes>>): Layout<Mes>() {
     constructor(vararg children: View<Mes>) : this(children.toList())
 }
 
-data class HorizontalLayout<Mes>(val children: List<View<Mes>>): View<Mes>() {
+data class HorizontalLayout<Mes>(val children: List<View<Mes>>): Layout<Mes>() {
     constructor(vararg children: View<Mes>) : this(children.toList())
 }
 
 data class TextField<Mes>(
     val label: String,
     val text: String,
-    val onInput: ((String) -> Mes)? = null
-): View<Mes>()
+    val onInput: ((String) -> Mes)? = null,
+    val fullWidth: Boolean = false
+): Element<Mes>()
 
 data class Checkbox<Mes>(
     val label: String,
     val isChecked: Boolean,
     val onClick: ((Boolean) -> Mes)? = null
-): View<Mes>()
+): Element<Mes>()
 
-data class GridLayout<Mes>(val children: List<List<View<Mes>>>): View<Mes>()
+data class GridLayout<Mes>(val children: List<List<View<Mes>>>): Layout<Mes>()
